3baf83e4f4597eda21ea2b962ab75935bbe7c247
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2011-10-17 21:55:46 +0000

    trivial eventbus usage

---
+++ JavaExpts/Guava/PublishSubscribe.java
@@ -0,0 +1,23 @@
+import com.google.common.eventbus.*;
+
+class Receiver {
+  String name;
+  @Subscribe
+  public void recieveMessage(String message) {
+    System.out.println(name + " received " + message);
+  }
+  public Receiver( String s ) {
+    name = s;
+  }
+}
+
+public class PublishSubscribe {
+  public static void main(String[] args) {
+    Receiver r1 = new Receiver("r1");
+    Receiver r2 = new Receiver("r2");
+    EventBus eb = new EventBus();
+    eb.register(r1);
+    eb.register(r2);
+    eb.post("Hi!");
+  }
+}
