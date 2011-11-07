77dc35591500296b93baeac720ddc0c4cb925d95
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2011-11-07 15:57:14 +0000

    cleaned up jukebox example

--- JavaExpts/Testing/TestJukebox.java
+++ JavaExpts/Testing/TestJukebox.java
@@ -1,37 +1,22 @@
 import junit.framework.*;
-// import static org.easymock.MockControl.*;
 import static org.easymock.EasyMock.*;
 
 public class TestJukebox extends TestCase {
   private Jukebox mockJukebox;
-  // private MockControl mockJukebox_control;
   private Jukebox mockJukebox_control;
 
   @Override
   protected void setUp() {
     // Create a control handle to the Mock object
-    // mockJukebox_control = MockControl.createControl(Jukebox.class);
     mockJukebox_control = createMock(Jukebox.class);
-    System.out.println("createMock returned " + mockJukebox_control.toString() );
 
     // And create the mock object itself
-    // mockJukebox = (Jukebox) mockJukebox_control.getMock();
     mockJukebox = mockJukebox_control;
   }
 
   public void testEasyMockDemo() {
     expect(mockJukebox.getCurrentSong()).andReturn("King Crimson -- Epitaph");
-    // set up the mock object by calling methods you
-    // want to exist
-    // mockJukebox.getCurrentSong();
-    // mockJukebox_control.setReturnValue("King Crimson -- Epitaph");
-    // expect("King Crimson -- Epitaph");
-
-    // you don't have to worry about the other dozen methods 
-    // defined in Jukebox
-
-    // switch from record to playback
-    // mockJukebox_control.replay();
+    // set up the mock object by calling methods you want to exist
     replay( mockJukebox_control );
 
     // now it's ready to use
