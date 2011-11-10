b72b391c2f18905113774a70e90febfd23400a91
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2011-11-10 19:08:53 +0000

    examples for easymock with less header comments

--- JavaExpts/JUnit/TestMp3Player.java
+++ JavaExpts/JUnit/TestMp3Player.java
@@ -1,39 +1,4 @@
 /**
- * Excerpted from the book, "Pragmatic Unit Testing in Java with JUnit"
- * ISBN 0-9745140-1-2
- * Copyright 2003 The Pragmatic Programmers, LLC.  All Rights Reserved.
- * Visit www.PragmaticProgrammer.com
- */
-
-/**
- * Excerpted from the book, "Pragmatic Unit Testing in Java with JUnit"
- * ISBN 0-9745140-1-2
- * Copyright 2003 The Pragmatic Programmers, LLC.  All Rights Reserved.
- * Visit www.PragmaticProgrammer.com
- */
-
-/**
- * Excerpted from the book, "Pragmatic Unit Testing in Java with JUnit"
- * ISBN 0-9745140-1-2
- * Copyright 2003 The Pragmatic Programmers, LLC.  All Rights Reserved.
- * Visit www.PragmaticProgrammer.com
- */
-
-/**
- * Excerpted from the book, "Pragmatic Unit Testing in Java with JUnit"
- * ISBN 0-9745140-1-2
- * Copyright 2003 The Pragmatic Programmers, LLC.  All Rights Reserved.
- * Visit www.PragmaticProgrammer.com
- */
-
-/**
- * Excerpted from the book, "Pragmatic Unit Testing in Java with JUnit"
- * ISBN 0-9745140-1-2
- * Copyright 2003 The Pragmatic Programmers, LLC.  All Rights Reserved.
- * Visit www.PragmaticProgrammer.com
- */
-
-/**
  * Excerpted from the book, "Pragmatic Unit Testing"
  * ISBN 0-9745140-1-2
  * Copyright 2003 The Pragmatic Programmers, LLC.  All Rights Reserved.
