fcb054d3cc9be7139f33a5bcc9e4f3f1cf86f122
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-02-03 17:14:27 +0000

    fixes to expect script

--- Scripts/factorial-iter.c
+++ Scripts/factorial-iter.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
-
-int computefactorial( int n ) {
-  if ( n == 0 ) {
-    return 1;
-  } else {
-    return n * computefactorial(n -1);
-  }
-}
-
 int main(int argc, char**argv){
+  int i;
   int n = -1;
   int factorial = 1;
   printf("Enter a number\n");
   scanf("%d", & n );
   printf("You entered %d\n", n);
   if ( n >= 0 ) {
-    factorial = computefactorial(n);
+    for( i = 1; i <= n; i++ ) {
+      factorial *= i;
+    }
     printf("The factorial of %d is %d\n", n, factorial );
   } else {
     printf("You entered a negative number: %d\n", n );
--- Scripts/print.sh
+++ Scripts/print.sh
@@ -1,8 +1,8 @@
 #!/bin/tcsh
-foreach f (`ls *.c`)
-  echo $f
-  enscript -fCourier6 -2r -G -p$f.ps $f
-  ps2pdf $f.ps
+foreach f (`ls *.c`) 
+  echo "Processing $f"
+  setenv TMP "`cat $f | grep main`"
+  setenv TMP2 `echo "$TMP" | sed '{s/main/foo/}' | sed '{s/\{//} | sed '{s/\}//}`
+  #setenv TMP2 `echo "$TMP" | sed '{s/main/foo/}'`
+  echo "$TMP2"
 end
-gs -dNOPAUSE -sDEVICE=pdfwrite \
- -sOUTPUTFILE=sources.pdf -dBATCH *.pdf
