b4f9af465fb5688b86b33a3449d676ec081c4a21
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-02-22 04:31:09 +0000

    options scraper

--- JavaExpts/HtmlUnit/Example.java
+++ JavaExpts/HtmlUnit/Example.java
@@ -13,14 +13,14 @@ public class Example  {
         WebDriver driver = new HtmlUnitDriver();
 
         // And now use this to visit Google
-        driver.get("http://finance.yahoo.com/q/op?s=qqq");
+        driver.get("http://finance.yahoo.com/q/op?s=qqq&m=2012-03");
+        System.out.println(driver.toString());
 
         // Find the text input element by its name
         List<WebElement> elements = driver.findElements(By.xpath("//*"));
         for (WebElement we : elements) {
           System.out.println("--- " + we.getText());
         }
-
 
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
