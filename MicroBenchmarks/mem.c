d50a68a7032e1341c3a458a2022bd19265a28fce
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-06-21 21:27:42 +0000

    messing around with multiple threads and mem BW

--- MicroBenchmarks/mem.c
+++ MicroBenchmarks/mem.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 
 #define N 100000000
 #define L 20
+
+extern void * task(void *);
+
+char *operation;
+int read;
 
 long A[N];
 
@@ -13,13 +20,35 @@ void init() {
 }
 
 int main(int argc, char **argv) {
-  char *operation = "reading";
 
-  int read = 1;
+  read = 1;
   if ( argc > 1 && !strcmp(argv[1], "write" ) ) {
     read = 0;
     operation = "writing";
   }
+  operation = "reading";
+
+  char *message1 = "Thread 1";
+  char *message2 = "Thread 2";
+
+  pthread_t thread1, thread2;
+  int iret1, iret2;
+  iret1 = pthread_create( &thread1, NULL, task, (void*) message1);
+  // iret2 = pthread_create( &thread2, NULL, task, (void*) message2);
+
+  /* Wait till threads are complete before main continues. Unless we  */
+  /* wait we run the risk of executing an exit which will terminate   */
+  /* the process and all threads before the threads have completed.   */
+  pthread_join( thread1, NULL);
+  // pthread_join( thread2, NULL);
+  printf("Thread 1 returns: %d\n",iret1);
+  printf("Thread 2 returns: %d\n",iret2);
+
+  exit(0);
+}
+
+
+void * task(void *dummy) {
 
   printf("size of long = %lu\n", sizeof(long) );
   printf("performing %d iterations, %.2e longs in each iter\n"
