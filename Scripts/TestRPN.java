a315aff932bff591071ca2c23ae5e9807db298d7
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-08-23 02:51:32 +0000

    afterclass report

--- Scripts/TestRPN.java
+++ Scripts/TestRPN.java
@@ -1,14 +1,37 @@
-import junit.framework.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
 import java.util.zip.DataFormatException;
-import java.util.EmptyStackException;
 
-public class TestRPN extends TestCase {
+// for querying results: http://www.dc.fi.udc.es/ai/tp/practica/junit/doc/cookbook/cookbook.htm
+// look for testresult object
 
-  @Override
-  protected void setUp() {
+public class TestRPN {
+
+  public static int score = 100;
+
+  @Before
+  public void setUp() {
+    // System.out.println("Setting up");
   }
 
-  public void testRPN() throws EmptyStackException, DataFormatException {
+  @AfterClass
+  public static void oneTimeTearDown() {
+    System.out.println("Final score is " + score );
+  }
+
+  @Test
+  public void testRPNBasic() throws DataFormatException {
     assertEquals( RPN.evalRPN("1 2 +"), 3 );
+  }
+
+  @Test
+  public void testRPNEmptyStack() throws DataFormatException {
+    try {
+      RPN.evalRPN("1 +");
+      fail("Should have thrown DataFormatException");
+    } catch (DataFormatException e) {
+      assertTrue(true);
+    }
   }
 }
