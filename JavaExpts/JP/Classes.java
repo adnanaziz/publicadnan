0ee7e972c1fc1be5739df014f9b6ce350752f5da
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-09-05 22:15:48 +0000

    messing with anonymous classes

--- JavaExpts/JP/Classes.java
+++ JavaExpts/JP/Classes.java
@@ -3,7 +3,7 @@ interface CustomCompare {
 }
 
 class CompareGpa implements CustomCompare {
-  int compare( Student s0, Student s1 ) {
+  public int compare( Student s0, Student s1 ) {
     if ( s0.GPA < s1.GPA ) {
       return -1;
     } else if ( s0.GPA > s1.GPA ) {
@@ -16,21 +16,88 @@ class CompareGpa implements CustomCompare {
 
 public class Classes {
 
+ static public class CompareName implements CustomCompare {
+   public int compare( Student s0, Student s1 ) {
+     System.out.println(s0 + " " + s1 + ":" + s0.name.compareTo(s1.name));
+     return s0.name.compareTo(s1.name);
+   }
+ }
+
+ public class CompareSchool implements CustomCompare {
+   public int compare( Student s0, Student s1 ) {
+     System.out.println(s0 + " " + s1 + ":" + s0.name.compareTo(s1.name));
+     return s0.school.compareTo(s1.school);
+   }
+ }
+
+ public void dummy() {
+    Student s0 = new Student( 3.8d, "Adnan Aziz", 123, "UT Austin");
+    Student s1 = new Student( 3.8d, "Imran Aziz", 543, "UT Arlington");
+    Student s2 = new Student( 3.9d, "Aardvark Smith", 459, "Berkeley");
+    Student s3 = new Student( 2.9d, "Thomas Jefferson", 453, "MIT");
+    Student [] testarray = {s0, s1, s2, s3};
+    Student [] sortedSchool = mysort( testarray, new CompareSchool() );
+    System.out.println("by schools");
+    for (Student s : sortedSchool ) {
+      System.out.println(s);
+    }
+ }
+
   public static void main(String [] args) {
-    String s0 = new String( 3.8, "Adnan Aziz", 123, "UT Austin");
-    String s1 = new String( 3.8, "Imran Aziz", 543, "UT Arlington");
-    String s2 = new String( 3.9, "Aardvark Smith", 459, "Berkeley");
-    String s2 = new String( 2.9, "Thomas Jefferson", 453, "MIT");
-    String [] testarray = {s0, s1, s2, s3};
+    new Classes().dummy();
+    Student s0 = new Student( 3.8d, "Adnan Aziz", 123, "UT Austin");
+    Student s1 = new Student( 3.8d, "Imran Aziz", 543, "UT Arlington");
+    Student s2 = new Student( 3.9d, "Aardvark Smith", 459, "Berkeley");
+    Student s3 = new Student( 2.9d, "Thomas Jefferson", 453, "MIT");
+    Student [] testarray = {s0, s1, s2, s3};
     // use generics for more
     // general case: http://stackoverflow.com/questions/3108182/
     // using-parameter-that-implements-multiple-interfaces-pre-generics
     CompareGpa cmpGpa = new CompareGpa();
-    Student [] sorted = mysort( Student [] studentArray, CustomCompare cmpGpa );
-    for (Student s : sorted ) {
+    Student [] sortedGpa = mysort( testarray, cmpGpa );
+    for (Student s : sortedGpa ) {
+      // System.out.println(s);
+    }
+    Student [] sortedId = mysort( testarray,
+      new CustomCompare() {
+        public int compare( Student s0, Student s1 ) {
+          if ( s0.id < s1.id ) {
+            return -1;
+          } else if ( s0.id > s1.id ) {
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+      }
+    );
+    for (Student s : sortedId ) {
+      // System.out.println(s);
+    }
+    Student [] sortedName = mysort( testarray, new CompareName() );
+    for (Student s : sortedName ) {
       System.out.println(s);
     }
   }
+
+  public static Student [] mysort( Student [] studentArray, CustomCompare cmpGpa ) {
+    Student [] result = new Student[studentArray.length];
+    for ( int i = 0 ; i < result.length; i++ ) {
+      result[i] = studentArray[i];
+    }
+    for ( int i = 0 ; i < result.length; i++ ) {
+      for ( int j = i ; j < result.length; j++ ) {
+        if ( cmpGpa.compare( result[j], result[i] ) == -1 ) {
+           System.out.println("swapping: " + i + "->" + j + result[j] + ":" + result[i] );
+           Student tmp = result[j];
+           result[j] = result[i];
+           result[i] = tmp;
+        }
+      }
+    }
+    return result;
+  }
+
 }
 
 class Student {
@@ -38,14 +105,15 @@ class Student {
   public String name;
   public int id;
   public String school;
-  public Student( double GPA, Student name, int id, Student school) {
+  public Student( double GPA, String name, int id, String school) {
     this.GPA = GPA;
     this.name = name;
     this.id = id;
     this.school = school;
   }
+  @Override 
+  public String toString() {
+    return name + "," + GPA + "," + id + "," + school;
+  }
 }
-
-
-
 
