2001041d81dbbc02a0947c00bae9755def96142a
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-09-06 17:11:08 +0000

    illustration of inner classes

--- JavaExpts/JP/Classes.java
+++ JavaExpts/JP/Classes.java
@@ -2,40 +2,31 @@ interface CustomCompare {
   int compare( Student s0, Student s1 );
 }
 
-class CompareGpa implements CustomCompare {
-  public int compare( Student s0, Student s1 ) {
-    if ( s0.GPA < s1.GPA ) {
-      return -1;
-    } else if ( s0.GPA > s1.GPA ) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-}
-
 public class Classes {
 
  static public class CompareName implements CustomCompare {
    public int compare( Student s0, Student s1 ) {
-     System.out.println(s0 + " " + s1 + ":" + s0.name.compareTo(s1.name));
      return s0.name.compareTo(s1.name);
    }
  }
 
  public class CompareSchool implements CustomCompare {
    public int compare( Student s0, Student s1 ) {
-     System.out.println(s0 + " " + s1 + ":" + s0.name.compareTo(s1.name));
      return s0.school.compareTo(s1.school);
    }
  }
 
- public void dummy() {
+ public static Student [] createTestArray() {
     Student s0 = new Student( 3.8d, "Adnan Aziz", 123, "UT Austin");
     Student s1 = new Student( 3.8d, "Imran Aziz", 543, "UT Arlington");
     Student s2 = new Student( 3.9d, "Aardvark Smith", 459, "Berkeley");
     Student s3 = new Student( 2.9d, "Thomas Jefferson", 453, "MIT");
     Student [] testarray = {s0, s1, s2, s3};
+    return testarray;
+ }
+
+ public void dummy() {
+    Student [] testarray = createTestArray();
     Student [] sortedSchool = mysort( testarray, new CompareSchool() );
     System.out.println("by schools");
     for (Student s : sortedSchool ) {
@@ -44,19 +35,28 @@ public class Classes {
  }
 
   public static void main(String [] args) {
-    new Classes().dummy();
-    Student s0 = new Student( 3.8d, "Adnan Aziz", 123, "UT Austin");
-    Student s1 = new Student( 3.8d, "Imran Aziz", 543, "UT Arlington");
-    Student s2 = new Student( 3.9d, "Aardvark Smith", 459, "Berkeley");
-    Student s3 = new Student( 2.9d, "Thomas Jefferson", 453, "MIT");
-    Student [] testarray = {s0, s1, s2, s3};
+    Student [] testarray = createTestArray();
+
+    class CompareGpa implements CustomCompare {
+      public int compare( Student s0, Student s1 ) {
+        if ( s0.GPA < s1.GPA ) {
+          return -1;
+        } else if ( s0.GPA > s1.GPA ) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    }
+
     // use generics for more
     // general case: http://stackoverflow.com/questions/3108182/
     // using-parameter-that-implements-multiple-interfaces-pre-generics
     CompareGpa cmpGpa = new CompareGpa();
     Student [] sortedGpa = mysort( testarray, cmpGpa );
+    System.out.println("by gpa");
     for (Student s : sortedGpa ) {
-      // System.out.println(s);
+      System.out.println(s);
     }
     Student [] sortedId = mysort( testarray,
       new CustomCompare() {
@@ -71,29 +71,35 @@ public class Classes {
         }
       }
     );
+    System.out.println("by id");
     for (Student s : sortedId ) {
-      // System.out.println(s);
+      System.out.println(s);
     }
+
+    System.out.println("by name");
     Student [] sortedName = mysort( testarray, new CompareName() );
     for (Student s : sortedName ) {
       System.out.println(s);
     }
+
+    new Classes().dummy();
   }
 
-  public static Student [] mysort( Student [] studentArray, CustomCompare cmpGpa ) {
+  public static Student [] mysort( Student [] studentArray, CustomCompare cmp ) {
     Student [] result = new Student[studentArray.length];
     for ( int i = 0 ; i < result.length; i++ ) {
       result[i] = studentArray[i];
     }
     for ( int i = 0 ; i < result.length; i++ ) {
+      int minindex = i;
       for ( int j = i ; j < result.length; j++ ) {
-        if ( cmpGpa.compare( result[j], result[i] ) == -1 ) {
-           System.out.println("swapping: " + i + "->" + j + result[j] + ":" + result[i] );
-           Student tmp = result[j];
-           result[j] = result[i];
-           result[i] = tmp;
+        if ( cmp.compare( result[j], result[minindex] ) < 0 ) {
+           minindex = j;
         }
       }
+      Student tmp = result[minindex];
+      result[minindex] = result[i];
+      result[i] = tmp;
     }
     return result;
   }
