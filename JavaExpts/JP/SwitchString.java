6f98a672ce27a8c670af39f8ad3ac5b67c7a4c0b
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-09-11 03:03:55 +0000

    switch on strings in java 1.7

---
+++ JavaExpts/JP/SwitchString.java
@@ -0,0 +1,13 @@
+public class SwitchString {
+  public static void main(String[] args) {
+    String s = "foo";
+    int i;
+    switch (s) {
+      case "bar": i = 1; break;
+      case "foo": i = 2; break;
+      case "widget": i = 3; break;
+      default: i = -1;
+    }
+    System.out.println("i = " + i );
+  }
+}
