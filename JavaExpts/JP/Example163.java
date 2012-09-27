f74ad56977b3a6f2722c9e2b36c6841d71f92faa
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-09-27 22:27:49 +0000

    added missing example

---
+++ JavaExpts/JP/Example163.java
@@ -0,0 +1,11 @@
+import java.lang.reflect.*;
+
+public class Example163 {
+  public static void main(String[] args) {
+    Method[] mod = C2.class.getMethods();
+    for (Method m: mod) {
+      if (Modifier.isPrivate(m.getModifiers())) 
+        System.out.println(m);
+    }
+  }
+}
