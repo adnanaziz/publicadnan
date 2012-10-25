6a06b2ee90bad130d1f0b9dc7107fbacc571fd6f
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-10-25 14:28:53 +0000

    small mods to specs, couple more tests

--- Labs/DPBacktrack/GenTicTacToe.java
+++ Labs/DPBacktrack/GenTicTacToe.java
@@ -5,7 +5,8 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.*;
 
 public class GenTicTacToe {
-  // the input s is of the form "X" or "O" indicating which player is to play next
+  // L is the length of the line that X or O have to make to win
+  // the input P is of the form "X" or "O" indicating which player is to play next
   // the input String... rows is a set of rows, each of which is a string
   // of the form "X,E,E,O". the rows are in order, e.g., when
   // winnable("X", "X,O,E", "E,E,O", "E,O,X") is called the corresponding
@@ -15,7 +16,14 @@ public class GenTicTacToe {
   // X O E
   // You can assume that the number of entries per row equals
   // the number of rows
-  public static boolean winnable(String s, String... rows ) {
+  public static boolean winnable(int L, String S, String... rows ) {
+  //TODO(EE422C): implement this function
+    return false;
+  }
+
+  // identical to function above, with K specifying the number
+  // of moves allowed for player s
+  public static boolean winnable(int L, String S, int K, String... rows ) {
   //TODO(EE422C): implement this function
     return false;
   }
--- Labs/DPBacktrack/TestLab.java
+++ Labs/DPBacktrack/TestLab.java
@@ -52,20 +52,51 @@ public class TestLab {
   @Test 
   public void testDcSimple() {
     List<Machine> machines = new ArrayList<Machine>();
-    machines.add( new Machine( 1000, 1000 ) );
-    machines.add( new Machine( 1000, 1000 ) );
+    machines.add( new Machine( 3, 3) );
+    machines.add( new Machine( 3, 3) );
     List<Task> tasks = new ArrayList<Task>();
-    tasks.add( new Task( 1000, 1000 ) );
-    tasks.add( new Task( 1000, 1000 ) );
-    tasks.add( new Task( 1000, 1000 ) );
+    tasks.add( new Task( 2, 2) );
+    tasks.add( new Task( 2, 1) );
+    tasks.add( new Task( 1, 2) );
+    tasks.add( new Task( 1, 1) );
     Map<Machine,List<Task>> result = DataCenter.solve( machines, tasks );
-    assertTrue( result == null );
+    assertTrue( result != null );
     score += 5;
   }
 
   @Test 
+  public void testDcComplexDirected() {
+    List<Machine> machines = new ArrayList<Machine>();
+    machines.add( new Machine( 1179, 1000 ) );
+    machines.add( new Machine( 1180, 1000 ) );
+
+    List<Task> tasks = new ArrayList<Task>();
+    int[] taskCpuArray = {65, 35, 245, 195, 65, 150, 275, 155, 120, 320, 75, 40, 200, 100, 220, 99};
+    int[] taskRamArray = {12, 13, 11,  8,   3,  14,  7,   2,   1,   5,   15, 12, 4,   9,   1,   12};
+    for ( int i = 0 ; i < taskCpuArray.length; i++ ) {
+      tasks.add( new Task( taskCpuArray[i], taskRamArray[i] ) );
+    }
+
+    Map<Machine,List<Task>> result = DataCenter.solve( machines, tasks );
+    assertTrue( result != null );
+    score += 10;
+  }
+
+  @Test 
   public void testTicTacToeSimple() {
-    assertFalse( GenTicTacToe.winnable("X", "X,O,X", "O,X,E", "E,O,E") ); 
+    assertTrue( GenTicTacToe.winnable(3, "X", "E,E,E", "O,X,E", "E,E,E") ); 
+    score += 5;
+  }
+
+  @Test 
+  public void testTicTacToeLimitedMovesSimple1() {
+    assertFalse( GenTicTacToe.winnable(3, "X", 2, "E,E,E", "O,X,E", "E,E,E") ); 
+    score += 5;
+  }
+
+  @Test 
+  public void testTicTacToeLimitedMovesSimple2() {
+    assertFalse( GenTicTacToe.winnable(3, "X", 3, "E,E,E", "O,X,E", "E,E,E") ); 
     score += 5;
   }
 }
