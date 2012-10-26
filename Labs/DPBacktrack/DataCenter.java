23cbad04918945a65fc01f6899f2e1bb8f3668cf
adnan.aziz <adnan.aziz@a257d59f-0c86-4a23-ac74-b4577f5186b5>
2012-10-26 15:41:14 +0000

    update, almost working DP

--- Labs/DPBacktrack/DataCenter.java
+++ Labs/DPBacktrack/DataCenter.java
@@ -21,20 +21,93 @@ public class DataCenter {
   // does not exceed those of the machine
   public static List<Task> solve(Machine machine, List<Task> tasks) {
   //TODO(EE422C): implement this function
-    List<Task> result = new ArrayList<Task>();
-    result.add( tasks.get(0) );
+    
+    Integer [][][] cache = new Integer[1+machine.getMips()][][];
+    Boolean [][][] keepcache = new Boolean[1+machine.getMips()][][];
+    for ( int i = 0 ; i < 1+machine.getMips(); i++ ) {
+      cache[i] = new Integer[1+machine.getRam()][];
+      keepcache[i] = new Boolean[1+machine.getRam()][];
+    }
+    for ( int i = 0 ; i < 1+machine.getMips(); i++ ) {
+      for ( int j = 0 ; j < 1+machine.getRam(); j++ ) {
+        cache[i][j] = new Integer[tasks.size()];
+        keepcache[i][j] = new Boolean[tasks.size()];
+        // not needed, since intialized to null
+        // for ( int k = 0 ; k < tasks.size(); k++ ) {
+        //   cache[i][j][k] = null;
+        // }
+      }
+    }
+    
+
+    int mostValue = solve( machine.getMips(), machine.getRam(), cache, keepcache, tasks, tasks.size() - 1 );
+
+    System.out.println("mostValue = " + mostValue );
+    List<Task> result = computeKeeps( machine.getMips(), machine.getRam(), keepcache, tasks, tasks.size() - 1 );
+
     return result;
+  }
+
+  static List<Task> computeKeeps( int mipsCapacity, int ramCapacity, Boolean[][][] keepcache, List<Task> tasks, int n) {
+    List<Task> result = new ArrayList<Task>();
+    int tmpMips = mipsCapacity;
+    int tmpRam = ramCapacity;
+    for ( int i = n-1 ; i >=0; i--) {
+      Boolean [][] foo = keepcache[tmpMips];
+      Boolean [] bar = keepcache[tmpMips][tmpRam];
+      if ( (null != keepcache[tmpMips][tmpRam][i] && ( keepcache[tmpMips][tmpRam][i]) ) )  {
+        result.add( tasks.get(i) );
+        tmpMips -= tasks.get(i).getMips();
+        tmpRam -= tasks.get(i).getRam();
+      }
+    }
+    return result;
+  }
+
+  static int solve( int mipsCapacity, int ramCapacity,
+                    Integer [][][] cache, Boolean [][][] keepcache, List<Task> tasks, int n) {
+    System.out.println("solve call with : " + mipsCapacity + "," + ramCapacity + "," + n);
+    if ( n == -1 ) {
+      return 0;
+    }
+    Task task = tasks.get(n);
+    if ( task.getMips() > mipsCapacity || task.getRam() > ramCapacity ) {
+      return 0;
+    }
+    if (cache[mipsCapacity][ramCapacity][n] != null) {
+      return cache[mipsCapacity][ramCapacity][n];
+    }
+
+
+    int bestWithout = solve( mipsCapacity, ramCapacity, cache, keepcache, tasks, n-1);
+    int bestWith = task.getPrice() + solve( mipsCapacity -task.getMips(), ramCapacity - task.getRam(), cache, keepcache, tasks, n-1);
+    int best;
+    if ( bestWith > bestWithout) {
+       best = bestWith;
+       keepcache[mipsCapacity][ramCapacity][n] = true;
+    } else {
+      best = bestWithout;
+      keepcache[mipsCapacity][ramCapacity][n] = false;
+    }
+    System.out.println("Nonbase return: " + best );
+    cache[mipsCapacity][ramCapacity][n] = best;
+    return best;
   }
 }
 
 class Machine {
-  int mips;
-  int ram;
+  private int mips;
+  private int ram;
 
   public Machine( int mips, int ram ) {
     this.mips = mips;
     this.ram = ram;
   }
+
+  public int getRam() { return ram; }
+  public int getMips() { return mips; }
+
+  
 }
 
 class Task {
@@ -82,6 +155,11 @@ class Task {
     this.mips = mips;
     this.ram = ram;
     this.price = price;
+  }
+
+  @Override
+  public String toString() {
+    return "" + mips + "," + ram + "," + price;
   }
 
   @Override
--- Labs/DPBacktrack/TestLab.java
+++ Labs/DPBacktrack/TestLab.java
@@ -51,19 +51,20 @@ public class TestLab {
     tasks.add( new Task.TaskBuilder().setMips(1).setRam(1).setPrice(1).build());
     tasks.add( new Task.TaskBuilder().setMips(1).setRam(1).setPrice(1).build());
     List<Task> result = DataCenter.solve( machine, tasks );
-    assertTrue( result.size() == 1 );
-    assertTrue( result.get(0).equals( new Task.TaskBuilder().setMips(2).setRam(1).setPrice(3).build() ) );
+    System.out.println("result = " + result.toString() );
+    assertEquals( result.size(), 1 );
+    assertEquals( tasksValue( result), 3 );
     score += 5;
   }
 
   @Test 
   public void testDcComplexDirected() {
-    Machine machine = new Machine( 1179, 1000 );
+    Machine machine = new Machine( 2, 2 );
 
     List<Task> tasks = new ArrayList<Task>();
-    int[] taskCpuArray =   {65, 35, 245, 195, 65, 150, 275, 155, 120, 320, 75, 40, 200, 100, 220, 99};
-    int[] taskRamArray =   {12, 13, 11,  8,   3,  14,  7,   2,   1,   5,   15, 12, 4,   9,   1,   12};
-    int[] taskPriceArray = {1,  1,  1,   1,   1,  1,   1,   1,   1,   1,   1,  1,  1,   1,   1,   1};
+    int[] taskCpuArray =   {1,1,1,1};
+    int[] taskRamArray =   {1,1,1,1};
+    int[] taskPriceArray = {1,2,3,4};
     for ( int i = 0 ; i < taskCpuArray.length; i++ ) {
       tasks.add( new Task.TaskBuilder()
                             .setMips(taskCpuArray[i])
@@ -72,23 +73,38 @@ public class TestLab {
     }
 
     List<Task> result = DataCenter.solve( machine, tasks );
-    assertTrue( result != null );
+    System.out.println("result = " + result.toString() );
+    assertEquals( 7, tasksValue(result) );
     score += 10;
   }
 
-  @Test 
+  static String tasksToString(List<Task> list) {
+    StringBuilder result = new StringBuilder();
+    for ( Task t : list )
+      result.append(t.toString() + ":");
+    return result.toString();
+  }
+
+  static int tasksValue(List<Task> list) {
+    int result = 0;
+    for ( Task t : list )
+      result += t.getPrice();
+    return result;
+  }
+
+  @Ignore @Test 
   public void testTicTacToeSimple() {
     assertTrue( GenTicTacToe.winnable(3, "X", "E,E,E", "O,X,E", "E,E,E") ); 
     score += 5;
   }
 
-  @Test 
+  @Ignore @Test 
   public void testTicTacToeLimitedMovesSimple1() {
     assertFalse( GenTicTacToe.winnable(3, "X", 2, "E,E,E", "O,X,E", "E,E,E") ); 
     score += 5;
   }
 
-  @Test 
+  @Ignore @Test 
   public void testTicTacToeLimitedMovesSimple2() {
     assertFalse( GenTicTacToe.winnable(3, "X", 3, "E,E,E", "O,X,E", "E,E,E") ); 
     score += 5;
