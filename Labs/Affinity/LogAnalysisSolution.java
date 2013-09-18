4e6944925c481cd41c571077fb370e1b85c9fdb3
Adnan Aziz <adnan.aziz@gmail.com>
2013-09-18 13:26:36 -0500

    log analysis solution & more tests

---
+++ Labs/Affinity/LogAnalysisSolution.java
@@ -0,0 +1,21 @@
+public class LogAnalysis {
+
+    // TODO(DAP): add helper methods, fields here
+
+  /**
+   * Take a string represention of a log file and return the pair
+   * of files that have the highest affinity.
+   * 
+   * You can assume there's no commas in file names, and that the logFileString 
+   * is valid.
+   *
+   * @param logFileString is a string holding lines of log entries
+   * @see LogAnalysisTest
+   * @return pair of String names ordered by dictionary order, separated by a comma, 
+   * e.g., articles/finance/gs-2013.html,reports/oil/ms-9/9/2013.pdf. 
+   */
+  public String highestAffinityPair(String logFileString) {
+    // TODO(DAP):write this method
+    return null;
+  }
+}
