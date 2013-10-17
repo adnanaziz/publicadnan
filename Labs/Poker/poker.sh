80b66386de16dfe15c53b96d524931c4209973c3
Adnan Aziz <adnan.aziz@gmail.com>
2013-10-17 08:46:48 -0500

    poker with emma coverage

--- Labs/Poker/poker.sh
+++ Labs/Poker/poker.sh
@@ -2,4 +2,6 @@
 CP=/usr/share/java/junit.jar:.
 javac -cp $CP PPP.java PPPTest.java Poker5Card.java Poker5CardTest.java 
 java -cp $CP org.junit.runner.JUnitCore Poker5CardTest
-java -cp $CP org.junit.runner.JUnitCore PPPTest
+# java -cp $CP org.junit.runner.JUnitCore PPPTest
+
+java -cp /usr/share/java/junit.jar:emma.jar emmarun -r html -cp /usr/share/java/junit.jar:. -sp . org.junit.runner.JUnitCore Poker5CardTest
