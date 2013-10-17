d059979723307f819a978cd7bbb3d61f4c3952dd
Adnan Aziz <adnan.aziz@gmail.com>
2013-10-17 08:58:57 -0500

    added iter version for change maker, and notes on setting stack size

--- classes/dap-13/examples/ChangeMaker.java
+++ classes/dap-13/examples/ChangeMaker.java
@@ -1,11 +1,25 @@
 import java.util.Map;
 import java.util.HashMap;
 
-
 public class ChangeMaker {
 	final static int[] denoms = {1,3,6,12,24,30};
 	// static Map<Integer,Integer> cache = new HashMap<Integer,Integer>();
 	static int[] cache;
+
+	static int mincoins_iter(int K) {
+		int[] values = new int[K+1];
+		for ( int i = 1 ; i <= K; i++ ) {
+			values[i] = Integer.MAX_VALUE;
+			for ( int c : denoms ) {
+				if ( c <= i ) {
+					if ( 1 + values[i - c] < values[i]) {
+						values[i] = 1 + values[i - c];
+					}
+				}
+			}
+		}
+		return values[K];
+	}
 	
 	static int mincoins(int K) {
 		cache = new int[K+1];
@@ -22,7 +36,7 @@ public class ChangeMaker {
 			return cache[K];
 		}
 		
-		System.out.println("K = " + K);
+		// System.out.println("K = " + K);
 		int minSoFar = Integer.MAX_VALUE;
 		for ( int c : denoms) {
 			if ( c > K ) {
--- classes/dap-13/examples/ChangeMakerTest.java
+++ classes/dap-13/examples/ChangeMakerTest.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+// Need 10MB stack to run, set using -Xss10M
 
 public class ChangeMakerTest {
 
@@ -9,11 +9,15 @@ public class ChangeMakerTest {
 	public void t1() {
 		int res = ChangeMaker.mincoins(1);
 		assertEquals(1,res);
+		res = ChangeMaker.mincoins_iter(1);
+		assertEquals(1,res);
 	}
 
 	@Test 
 	public void t2() {
 		int res = ChangeMaker.mincoins(2);
+		assertEquals(2,res);		
+		res = ChangeMaker.mincoins_iter(2);
 		assertEquals(2,res);		
 	}
 	
@@ -21,11 +25,23 @@ public class ChangeMakerTest {
 	public void t25() {
 		int res = ChangeMaker.mincoins(25);
 		assertEquals(2,res);
+		res = ChangeMaker.mincoins_iter(25);
+		assertEquals(2,res);
 	}
 	
 	@Test 
 	public void t3000() {
 		int res = ChangeMaker.mincoins(3000);
 		assertEquals(100,res);
+		res = ChangeMaker.mincoins_iter(3000);
+		assertEquals(100,res);
+	}
+
+	@Test 
+	public void t30000() {
+		int res = ChangeMaker.mincoins(30000);
+		assertEquals(1000,res);
+		res = ChangeMaker.mincoins_iter(30000);
+		assertEquals(1000,res);
 	}
 }
