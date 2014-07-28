6ce437c3e69d0505268089cac3900a21b55f7bf2
adnanaziz <adnan.aziz@gmail.com>
2014-07-28 11:42:46 -0500

    added shell return code to test

--- python/scraping-lab/test1.py
+++ python/scraping-lab/test1.py
@@ -8,5 +8,6 @@ if json.loads(computedJson) != json.loads(expectedJson):
   print "Test failed!"
   print "Expected output:", expectedJson
   print "Your output:", computedJson
+  assert False
 else:
   print "Test passed"
