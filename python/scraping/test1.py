d53c302e0069e2b57ebc8b96b0261969a318db14
Adnan Aziz <adnan.aziz@gmail.com>
2014-08-01 16:26:25 -0500

    adding test to scraping folder

---
+++ python/scraping/test1.py
@@ -0,0 +1,13 @@
+import json
+import yahoo_options_data
+
+computedJson = yahoo_options_data.contractAsJson("f.dat")
+expectedJson = open("f.json").read()
+
+if json.loads(computedJson) != json.loads(expectedJson):
+  print "Test failed!"
+  print "Expected output:", expectedJson
+  print "Your output:", computedJson
+  assert False
+else:
+  print "Test passed"
