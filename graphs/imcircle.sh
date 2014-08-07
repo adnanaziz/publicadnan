43aab25e32a77c64b3e561e251ae7bc62b89663d
Adnan Aziz <adnan.aziz@gmail.com>
2014-08-06 23:18:21 -0500

    experiments with graphviz

---
+++ graphs/imcircle.sh
@@ -0,0 +1,6 @@
+convert adnan-new-90.png -alpha set -gravity center -extent 120x120 \
+          badge_mask.png -compose DstIn -composite \
+          badge_shading.png -compose Over -composite \
+          badge_trans_bg.png
+
+#convert -size 200x200 xc:none -fill adnan-new.jpg -draw "circle 50,50 100,1" circle_thumb.png
---
+++ python/tmp/keepalive.py
@@ -0,0 +1,11 @@
+from my_functions import g
+
+def f(x, y):
+    return g(x + y)
+
+def main(): 
+    x= 6
+    y = 7.5
+    result = x + y
+
+if __name__ == '__main__': main()
