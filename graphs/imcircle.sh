ae736e95a77220de0ba877d4581c2468fd70ddc0
Adnan Aziz <adnan.aziz@gmail.com>
2014-08-08 10:07:12 -0500

    small changes for graph

--- graphs/imcircle.sh
+++ graphs/imcircle.sh
@@ -1,6 +1,7 @@
-convert adnan-new-90.png -alpha set -gravity center -extent 120x120 \
-          badge_mask.png -compose DstIn -composite \
-          badge_shading.png -compose Over -composite \
-          badge_trans_bg.png
+dot -Kneato -v -Tpng graph.dot -o graph.png
+#convert adnan-new-90.png -alpha set -gravity center -extent 120x120 \
+#          badge_mask.png -compose DstIn -composite \
+#          badge_shading.png -compose Over -composite \
+#          badge_trans_bg.png
 
 #convert -size 200x200 xc:none -fill adnan-new.jpg -draw "circle 50,50 100,1" circle_thumb.png
