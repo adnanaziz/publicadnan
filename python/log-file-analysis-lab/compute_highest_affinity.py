1b947bd5df0d88f316104bcdb1cfe73bfecad543
adnanaziz <adnan.aziz@gmail.com>
2014-08-20 10:40:23 -0500

    added skeleton code for lab

---
+++ python/log-file-analysis-lab/compute_highest_affinity.py
@@ -0,0 +1,12 @@
+# No need to process files and manipulate strings - we will
+# pass in lists (of equal length) that correspond to 
+# sites views. The first list is the site visited, the second is
+# the user who visited the site.
+
+# See the test cases for more details.
+
+def highest_affinity(site_list, user_list):
+  # Returned string pair should be ordered by dictionary order
+  # I.e., if the highest affinity pair is "foo" and "bar"
+  # return ("bar", "foo"). 
+  return ('abc', 'def')
