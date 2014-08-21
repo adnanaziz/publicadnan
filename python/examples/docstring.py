a1ce6115dc2906baa0c3b960cb9b9c659331ed30
adnanaziz <adnan.aziz@gmail.com>
2014-08-21 15:48:36 -0500

    doc string example

---
+++ python/examples/docstring.py
@@ -0,0 +1,16 @@
+'''This is a module to 
+illustrate the docstring facility
+'''
+
+class Aclass(object):
+  'This is where the class description goes'
+
+  def aMethod(self):
+    "This method returns 42"
+    return 42
+
+
+def aFun(x,y):
+  '''This function forms the product of x and y'''
+  return x * y
+
---
+++ python/examples/docstringtest.py
@@ -0,0 +1,17 @@
+import docstring
+
+print docstring.__doc__
+
+print docstring.Aclass.__doc__
+
+print docstring.Aclass.aMethod.__doc__
+
+print docstring.aFun.__doc__
+
+print abs.__doc__
+
+print range.__doc__
+
+import random
+
+print random.randint.__doc__
