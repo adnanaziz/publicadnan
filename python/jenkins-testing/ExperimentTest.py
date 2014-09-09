5b3072336a50a276f3adbc98240651c48024324a
Adnan Aziz <adnan.aziz@gmail.com>
2014-09-09 14:01:51 -0500

    duplicate largest element test

--- python/jenkins-testing/ExperimentTest.py
+++ python/jenkins-testing/ExperimentTest.py
@@ -10,6 +10,11 @@ class MyTestCase(unittest.TestCase):
   def test_t2(self):
     r1 = Experiment.largest([3,2,1]) 
     self.assertEqual(r1, 3)
+
+  def test_t3(self):
+    r1 = Experiment.largest([3,2,3]) 
+    self.assertEqual(r1, 3)
+ 
  
 if __name__ == '__main__':
     unittest.main()
