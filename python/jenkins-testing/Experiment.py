19a1ef7fe92df7a84286876398a8cb6e45084f76
Adnan Aziz <adnan.aziz@gmail.com>
2014-09-09 14:04:55 -0500

    fix off by one in largest()

--- python/jenkins-testing/Experiment.py
+++ python/jenkins-testing/Experiment.py
@@ -1,8 +1,8 @@
 import sys
 
 def largest(list):
-  max=0# initialize max to 0
-  for index in range(len(list)-1):
+  max = 0
+  for index in range(len(list)):
    if (list[index] > max):
      max = list[index]
   return max
