418ccb56d5ad9fce588cedf4929f54eb498f4964
Adnan Aziz <adnan.aziz@gmail.com>
2014-09-09 14:01:20 -0500

    try reverse sorted list

--- python/jenkins-testing/ExperimentTest.py
+++ python/jenkins-testing/ExperimentTest.py
@@ -6,6 +6,10 @@ class MyTestCase(unittest.TestCase):
   def test_t1(self):
     r1 = Experiment.largest([1,1,1]) 
     self.assertEqual(r1, 1)
+
+  def test_t2(self):
+    r1 = Experiment.largest([3,2,1]) 
+    self.assertEqual(r1, 3)
  
 if __name__ == '__main__':
     unittest.main()
