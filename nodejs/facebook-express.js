28ee7456b4077a9e528cb00f49e780eb5e9318bd
Adnan Aziz <adnan.aziz@gmail.com>
2015-05-22 21:00:03 -0500

    commenting auth

--- nodejs/facebook-express.js
+++ nodejs/facebook-express.js
@@ -1,3 +1,30 @@
+var winston = require('winston');
+winston.setLevels({
+    debug: 0,
+    info: 1,
+    silly: 2,
+    warn: 3,
+    error: 4,
+});
+winston.addColors({
+    debug: 'green',
+    info: 'cyan',
+    silly: 'magenta',
+    warn: 'yellow',
+    error: 'red'
+});
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {
+    level: 'debug',
+    colorize: true
+});
+
+winston.debug("debug");
+winston.info("info");
+winston.silly("silly");
+winston.warn("warn");
+
+
 var async = require('async');
 var graph = require('fbgraph');
 var fbAppId = process.env.FB_APP_ID;
@@ -81,20 +108,24 @@ app.use(function(req, res, next) {
 
 app.use(express.static('.'));
 
+// to auth package
 passport.serializeUser(function(user, done) {
-    console.log("serializeUser, id = ", user.id);
+    winston.info("serializeUser, id = ", user.id);
     done(null, user.id);
 });
 
+// to auth package
 passport.deserializeUser(function(id, done) {
-    console.log("deserializeUser, id = ", id);
+    winston.info("deserializeUser, id = ", id);
     User.findById(id, function(err, user) {
         done(err, user);
     });
 });
 
+// to auth package
 var FacebookStrategy = require('passport-facebook').Strategy;
 
+// to auth package
 var User = require('./User');
 
 
@@ -118,27 +149,20 @@ var User = require('./User');
 /**
  * Login Required middleware.
  */
-exports.isAuthenticated = function(req, res, next) {
-    console.log("in isAuthenticated()");
+// to auth package, exported
+exports.isCustomAuthenticated = function(req, res, next) {
+    winston.info("in isAuthenticated()");
 
     if (req.body !== undefined && (req.body.FBAppScopeId !== undefined && (req.body.loginCookie !== undefined || req.body.adminPassword !== undefined))) {
         // it's an ios call
-        console.log("isAuthenticated, body = " + JSON.stringify(req.body, null, 4));
         if (req.body.adminPassword !== undefined && req.body.adminPassword == "bathtub") {
-            console.log("admin login");
+            winston.info("admin login");
             return next();
         } else {
+            winston.info("isAuthenticated, waterfall; body = " + JSON.stringify(req.body, null, 4));
             async.waterfall(
-                [function(cb1) {
-                        if (req.body !== undefined) {
-                            console.log("isAuthenticated, body = " + JSON.stringify(req.body, null, 4));
-                            if (req.body.adminPassword !== undefined && req.body.adminPassword == "bathtub") {
-                                console.log("admin login");
-                                return next();
-                            }
-                            cb1();
-                        }
-                    },
+                [
+
                     function(cb2) {
                         User.findOne({
                                 facebook: req.body.FBAppScopeId
@@ -157,11 +181,13 @@ exports.isAuthenticated = function(req, res, next) {
                                     return;
                                 }
                                 // all checks pass
+                                req.user = user; // trying to mimic passport
                                 cb2(null);
                             });
                     }
                 ],
                 function(err) {
+                    winston.info("in final callback, err = ", err);
                     if (err) {
                         res.json({
                             code: 500,
@@ -173,27 +199,31 @@ exports.isAuthenticated = function(req, res, next) {
                 }
             );
         }
-    } else {
-        // not an api request, coming from a browser, so let passport handle it
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.redirect('/login.html');
     }
+};
+
+// to auth package, exported
+exports.isPassportAuthenticated = function(req, res, next) {
+    // not an api request, coming from a browser, so let passport handle it
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login.html');
 };
 
 /**
  * Authorization Required middleware.
  */
+// to auth package, exported. likely unused?
 exports.isAuthorized = function(req, res, next) {
     var provider = req.path.split('/').slice(-1)[0];
 
-    console.log("isAuthorized, req.path = " + req.path);
-    console.log("isAuthorized, provider = " + provider);
+    winston.info("isAuthorized, req.path = " + req.path);
+    winston.info("isAuthorized, provider = " + provider);
 
     if (_.find(req.user.tokens, {
-            kind: provider
-        })) {
+        kind: provider
+    })) {
         next();
     } else {
         res.redirect('/auth/' + provider);
@@ -203,25 +233,26 @@ exports.isAuthorized = function(req, res, next) {
 /**
  * Sign in with Facebook.
  */
+// to auth package
 passport.use(new FacebookStrategy(secrets.facebook, function(req, accessToken, refreshToken, profile, done) {
-    console.log("0. accessToken = " + accessToken);
-    console.log("0. refreshToken = " + refreshToken);
-    console.log("0. profile = " + JSON.stringify(4, null, profile));
-    console.log("0. profile.id = " + profile.id);
+    winston.info("0. accessToken = " + accessToken);
+    winston.info("0. refreshToken = " + refreshToken);
+    winston.info("0. profile = " + JSON.stringify(4, null, profile));
+    winston.info("0. profile.id = " + profile.id);
     if (req.user) {
-        console.log("fbs 1. req.user is true");
+        winston.info("fbs 1. req.user is true");
         User.findOne({
             facebook: profile.id
         }, function(err, existingUser) {
             if (existingUser) {
-                console.log("fbs 2. existingUser is true, err = " + err);
+                winston.info("fbs 2. existingUser is true, err = " + err);
                 req.flash('errors', {
                     msg: 'There is already a Facebook account that belongs to you.' +
                         'Sign in with that account or delete it, then link it with your current account.'
                 });
                 done(err);
             } else {
-                console.log("fbs 3. existingUser is false");
+                winston.info("fbs 3. existingUser is false");
                 User.findById(req.user.id, function(err, user) {
                     user.facebook = profile.id;
                     user.tokens.push({
@@ -243,15 +274,15 @@ passport.use(new FacebookStrategy(secrets.facebook, function(req, accessToken, r
             }
         });
     } else {
-        console.log("fbs 4. req.user is false");
+        winston.info("fbs 4. req.user is false");
         User.findOne({
             facebook: profile.id
         }, function(err, existingUser) {
             if (existingUser) {
-                console.log("fbs 5. - existing user, user = " + existingUser);
+                winston.info("fbs 5. - existing user, user = " + existingUser);
                 return done(null, existingUser);
             }
-            console.log("fbs 6. no existing user");
+            winston.info("fbs 6. no existing user");
             User.findOne({
                 email: profile._json.email
             }, function(err, existingEmailUser) {
@@ -263,7 +294,7 @@ passport.use(new FacebookStrategy(secrets.facebook, function(req, accessToken, r
                     });
                     done(err);
                 } else {
-                    console.log("fbs 7. new User()");
+                    winston.info("fbs 7. new User()");
                     var user = new User();
                     user.email = profile._json.email;
                     user.facebook = profile.id;
@@ -286,55 +317,45 @@ passport.use(new FacebookStrategy(secrets.facebook, function(req, accessToken, r
     }
 }));
 
-app.use(function(req, res, next) {
-    if (/api/i.test(req.path)) req.session.returnTo = req.path;
-    next();
-});
 
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+    winston.info('Example app listening at http://%s:%s', host, port);
 });
 
+// with app.js
+app.use(function(req, res, next) {
+    if (/api\/browser/i.test(req.path)) req.session.returnTo = req.path;
+    next();
+});
+
+// with app.js
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
     failureRedirect: '/login.html'
 }), function(req, res) {
-    console.log('req.session.returnTo: ' + req.session.returnTo);
+    winston.info('req.session.returnTo: ' + req.session.returnTo);
     res.redirect(req.session.returnTo || '/');
 });
 
-app.all('/api/userdata', exports.isAuthenticated, function(req, res) {
-    console.log('in userdata');
-    res.json({
-        msg: "it worked",
-        user: req.user
-    });
-});
-
-app.get('/logout', exports.isAuthenticated, function(req, res) {
+// two logout functions
+app.get('/logout', exports.isPassportAuthenticated, function(req, res) {
     req.logout();
     res.redirect('logout.html');
 });
 
 // err implies user did not have right accessToken
-var updateFbUser = function(res, err, existingFbUser, facebookuser) {
-    console.log("updateFbUser: " + existingFbUser + " " + err + " " + facebookuser);
-    if (err) {
-        res.json({
-            code: 500,
-            msg: err
-        });
-        return;
-    }
+// to auth package
+var updateFbUser = function(existingFbUser, facebookuser, callback) {
     var user;
     // if we're here, he had a valid auth token/FBAppScopeId
     // TODO: use crypto to create right cookie
     var loginCookie = "cookie_" + facebookuser.FBAppScopeId;
     if (existingFbUser !== null) {
         // update the user
+        //TODO: dups code in next block, use iter over facebookuser
         user = existingFbUser;
         user.email = facebookuser.email;
         user.profile = {
@@ -356,20 +377,14 @@ var updateFbUser = function(res, err, existingFbUser, facebookuser) {
 
     user.save(function(err, storeduser) {
         if (err) {
-            console.log("updateFbUserRes err:" + err);
-            res.json({
-                code: 500,
-                msg: err
-            });
-            return;
+            winston.info("updateFbUserRes err:" + err);
+            return callback(err);
         }
-        res.json({
-            code: 200,
-            loginCookie: loginCookie
-        });
+        callback(null, loginCookie);
     });
 };
 
+// to auth package
 var options = {
     timeout: 3000,
     pool: {
@@ -380,65 +395,79 @@ var options = {
     }
 };
 
-
-var getUserData = function(accessToken, existingUser, updateFbUserRes) {
+// to auth package
+var getUserData = function(accessToken, existingUser, callback) {
     // idea: err automatically acts as an invalid login. we dont care
     /// about the login cookie now, since accessToken trumps that
+    winston.info("accessToken =", accessToken);
+    winston.info("existingUser =", JSON.stringify(existingUser, null, 4));
     async.waterfall(
         [
+
             function(cb1) {
-                var url = "me" + "?fields=name,email&access_token=" + accessToken;
+                // TODO: why are website, location not coming? gender comes for doroty but not for adnan?
+                var url = "me" + "?fields=name,email,website,location,gender&access_token=" + accessToken;
                 graph.setOptions(options)
                     .get(url, cb1);
             },
             function(fbuser, cb2) {
-                console.log("2." + JSON.stringify(fbuser, null, 4));
+                winston.info("2." + JSON.stringify(fbuser, null, 4));
                 var url = "/picture?width=160&height=160&access_token=" + accessToken;
                 graph.get(url, function(err, profilepic) {
                     if (err) {
                         cb2(err);
                         return;
                     }
+                    winston.warn("fbuser = ", fbuser);
+                    //TODO: can we just clone?
                     var facebookuser = {
                         name: fbuser.name,
                         email: fbuser.email,
+                        location: fbuser.location,
+                        gender: fbuser.gender,
+                        website: fbuser.website,
                         id: fbuser.id,
                         pictureUrl: profilepic.location
                     }
-                    cb2(null, existingUser, facebookuser);
+                    winston.warn("callback with existingUser, facebookuser:", JSON.stringify(existingUser), JSON.stringify(facebookuser));
+
+                    callback(null, existingUser, facebookuser);
                 });
             }
-        ],
-        updateFbUserRes
+        ]
     );
 
 };
 
+// to auth package
 var logoutUser = function(req, res) {
-    console.log("logoutUser: 1");
+    winston.info("logoutUser: 1");
     var request = req.body;
     if (request.FBAppScopeId === undefined) {
+        // use server response call
         res.json({
             code: 500,
             msg: "no FBAppScopeId " + FBAppScopeId
         });
         return;
     }
-    console.log("logoutUser: 2");
+    winston.info("logoutUser: 2");
     User.findOne({
             facebook: request.FBAppScopeId
         },
         function(err, user) {
             if (err) {
+                // use server response call
                 res.json({
                     code: 500,
                     msg: "logout user lookup error " + FBAppScopeId
                 });
                 return;
             }
-            console.log("logoutUser, user = " + user);
+            winston.info("logoutUser, user = " + user);
             user.loginCookie = "loggedOut";
             user.save(function(err, saveduser) {
+                // use server response call
                 if (err) {
                     res.json({
                         code: 500,
@@ -446,7 +475,8 @@ var logoutUser = function(req, res) {
                     });
                     return
                 }
-                console.log("logoutUser, save, succ = " + user);
+                winston.info("logoutUser, save, succ = " + user);
+                // use server response call
                 res.json({
                     code: 200,
                     msg: "succ logout"
@@ -456,7 +486,7 @@ var logoutUser = function(req, res) {
     );
 };
 
-
+// to auth package
 var createOrUpdateUser = function(req, res) {
     var request = req.body;
     if (request.FBAppScopeId === undefined) {
@@ -479,11 +509,36 @@ var createOrUpdateUser = function(req, res) {
                 });
                 return;
             }
-            getUserData(request.accessToken, user, async.apply(updateFbUser, res));
+            async.waterfall(
+                [
+                    async.apply(getUserData, request.accessToken, user),
+                    updateFbUser
+                ],
+                function(err, loginCookie) {
+                    if (err) {
+                        res.json({
+                            code: 500,
+                            msg: err
+                        });
+                        return;
+                    }
+                    res.json({
+                        code: 200,
+                        loginCookie: loginCookie
+                    });
+                });
         }
     );
 };
 
-// for ios requests.
+// for ios (or any programmatic) requests.
 app.post('/api/createOrUpdateFBUser', createOrUpdateUser);
-app.post('/api/logoutUser', exports.isAuthenticated, logoutUser);
+app.post('/api/logoutUser', exports.isCustomAuthenticated, logoutUser);
+app.post('/api/getUserData', exports.isCustomAuthenticated, function(req, res) {
+    res.json(req.user);
+});
+
+// for browser requests.
+app.get('/api/browser/getUserData', exports.isPassportAuthenticated, function(req, res) {
+    res.send(req.user);
+});
