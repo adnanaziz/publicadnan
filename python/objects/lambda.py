113aab1823384d7240a0ff2351ab152f2763eae7
adnanaziz <adnan.aziz@gmail.com>
2015-09-04 10:52:46 -0500

    lambda sort

--- python/objects/lambda.py
+++ python/objects/lambda.py
@@ -1,6 +1,9 @@
-B = [("aa", 100), ("A", 100), ("adnan",10), ("aa", 1000), ("b",5)]
+B = [("abc", 100), ("A", 100), ("adnan",10), ("aa", 1000), ("b",5), ("aa", 100) ]
 
-# lamda notation: don't need to define another function
+# note that python's A if B else C syntax is the equivalent of Java's
+# B ? A : C syntax
+
+# lambda notation: don't need to define another function
 mycmp =  lambda x,y: (x[1] - y[1]) if ( x[1] != y[1] ) else cmp(x[0], y[0])
 
 B.sort( mycmp )
