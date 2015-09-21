261edeaa977b5ce670b22c6a382603e2782926d6
adnanaziz <adnan.aziz@gmail.com>
2015-09-21 12:54:56 -0500

    cleaning up

--- JavaExpts/ExceptionTypes.java
+++ JavaExpts/ExceptionTypes.java
@@ -1,19 +1,24 @@
 import java.io.*;
+import java.util.*;
 
 public class ExceptionTypes {
 
   public static void doThis() {
     try {
-      NonRTException.entry();
       RTException.entry();
-    } catch (RuntimeException e) {
+      NonRTException.entry();
+    //} catch (RuntimeException e) {
+    //  e.printStackTrace();
+    } catch (IOException e) {
       e.printStackTrace();
-    // } catch (IOException e) {
-    //   e.printStackTrace();
     }
   }
 
   public static void main(String[] args) {
+    Integer x = 123;
+    List<Integer> y = new ArrayList<>();
+    if (x == y) {
+    }
     ExceptionTypes.doThis();
   }
 
